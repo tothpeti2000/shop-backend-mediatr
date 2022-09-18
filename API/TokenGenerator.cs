5c763b15d1f485924745d2ee0aa86c0594d6a1fc
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-09-18 14:14:17 +0200

    Add project files.

---
+++ API/TokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API
+{
+    public class TokenGenerator
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenGenerator(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateToken(string userId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(configuration["JWT:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId)
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
